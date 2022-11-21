fa8e9aa17344838ad8769c35577422ea5ecc66f0
h0wter <howter99@gmail.com>
2022-11-21 23:47:48 +0200

    add styles for links

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const Header = styled.header`
   padding: 10px;
+  margin-bottom: 10px;
   border: 1px solid #d5d9d9;
   box-shadow: rgba(213, 217, 217, 0.979) 0 5px 4px 0;
 `;
@@ -13,4 +15,26 @@ export const NavList = styled.ul`
 
 export const NavItem = styled.li`
   margin-right: 15px;
+`;
+
+export const Link = styled(NavLink)`
+  position: relative;
+  font-weight: 500;
+  text-decoration: none;
+
+  &:hover:after {
+    content: '';
+    position: absolute;
+    display: flex;
+    height: 2px;
+    width: 100%;
+    background-color: black;
+  }
+
+  &.active {
+    color: red;
+    &:after {
+      background-color: red;
+    }
+  }
 `;
